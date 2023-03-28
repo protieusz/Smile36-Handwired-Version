a98cb91a894567715ecd6433663b8f5ae822eeef
Protieus <118025702+protieusz@users.noreply.github.com>
2023-03-27 19:53:23 -0700

    updated thumbstick enable

--- smile36/keymaps/pspjoystick/rules.mk
+++ smile36/keymaps/pspjoystick/rules.mk
@@ -1,1 +1,9 @@
 SRC += analog.c
+
+THUMBSTICK_ENABLE = yes
+
+ifeq ($(strip $(THUMBSTICK_ENABLE)), yes)
+    POINTING_DEVICE_ENABLE = yes
+	POINTING_DEVICE_DRIVER = analog_joystick
+    OPT_DEFS += -DTHUMBSTICK_ENABLE
+endif
